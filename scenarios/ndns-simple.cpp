be41ff159db0080351f14ce0d08e75fc5731a81a
Steve DiBenedetto <dibenede@cs.colostate.edu>
2015-09-27 12:24:03 -0700

    add and parse test ndns config

--- scenarios/ndns-simple.cpp
+++ scenarios/ndns-simple.cpp
@@ -49,7 +49,7 @@ main(int argc, char* argv[])
   ndn::AppHelper producerHelper("NdnsServerApp");
   // Producer will reply to all requests starting with /prefix
   //producerHelper.SetPrefix("/prefix");
-  //producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
+  producerHelper.SetAttribute("ConfigFile", StringValue("/Users/dibenede/Sites/ndn/hackathon/ndnSIM/ndns-scenario/ndns.conf.sample"));
   producerHelper.Install(nodes.Get(2)); // last node
 
   Simulator::Stop(Seconds(20.0));
